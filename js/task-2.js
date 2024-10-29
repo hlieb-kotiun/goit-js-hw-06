2d683810f05b0d0adfef3d86e94ebdab318775d8
Hlieb Kotin <gleb.kotiun@gmaill.com>
2024-10-29 14:45:49 +0100

    homework 6, task 1 - 3

---
+++ js/task-2.js
@@ -0,0 +1,35 @@
+console.log("");
+console.log("Task-2");
+console.log("");
+
+class Storage {
+  #item;
+  constructor(lalala) {
+    this.#item = lalala;
+  }
+  getItems() {
+    return this.#item;
+  }
+  addItem(newItem) {
+    this.#item.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    const index = this.#item.indexOf(itemToRemove);
+    if (index === 1) {
+      return this.#item.splice(index, 1);
+    }
+    return;
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
---
+++ js/task-3.js
@@ -0,0 +1,31 @@
+console.log("");
+console.log("Task-3");
+console.log("");
+
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+  getValue() {
+    return this.#value;
+  }
+  padEnd(str) {
+    this.#value += str;
+  }
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
